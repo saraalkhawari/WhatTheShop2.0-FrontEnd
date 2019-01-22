af9c0cc3a3b848dd931cc5301c0eedffae0b113a
Lailz <lailaabdulraheem@gmail.com>
2019-01-22 20:58:36 +0300

    icons added and footer styled

--- navigation/BottomTab.js
+++ navigation/BottomTab.js
@@ -5,10 +5,41 @@ import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import LolStack from "./LolStack";
 import ProfileStack from "./ProfileStack";
 
-const BottomTab = createBottomTabNavigator({
-  ProfileTab: ProfileStack,
-  LolTab: LolStack
-});
+const BottomTab = createBottomTabNavigator(
+  {
+    ProfileTab: ProfileStack,
+    LolTab: LolStack
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "LolTab") {
+          iconName = "smiley";
+          iconType = "Octicons";
+        } else if (routeName === "ProfileTab") {
+          iconName = "person";
+          iconType = "MaterialIcons";
+        }
+        return (
+          <Icon name={iconName} style={{ color: tintColor }} type={iconType} />
+        );
+      }
+    }),
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: "#6200EE",
+      inactiveTintColor: "#858585",
+      style: {
+        backgroundColor: "white"
+      },
+      labelStyle: {
+        fontSize: 12
+      }
+    }
+  }
+);
 
 const AppContainer = createAppContainer(BottomTab);
 
--- navigation/ProfileStack.js
+++ navigation/ProfileStack.js
@@ -1,7 +1,10 @@
 import { createStackNavigator } from "react-navigation";
 
 import ProfileScreen from "../components/Profile";
+import React from "react";
+
 import LoginScreen from "../components/Login";
+import { Icon } from "native-base";
 const ProfileStack = createStackNavigator(
   {
     Profile: ProfileScreen,
