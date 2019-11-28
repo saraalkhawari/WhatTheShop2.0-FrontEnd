93df63ad9c247be5b16e76cecd89185bd71b8ba1
Sarah Al Bader <s.r.albader@gmail.com>
2019-11-28 18:07:01 +0300

    added ProfileStack(StackNav) to SwitchNav

--- navigation/SwitchNav.js
+++ navigation/SwitchNav.js
@@ -1,9 +1,11 @@
 import { createSwitchNavigator } from "react-navigation";
 
+import ProfileStack from "../navigation/ProfileStack";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
 
 const SwitchNav = createSwitchNavigator({
+  ProfileStack: ProfileStack,
   Signup: Signup,
   Login: Login
 });
