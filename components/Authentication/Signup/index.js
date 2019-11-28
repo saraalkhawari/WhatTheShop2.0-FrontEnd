06ace476ec9f2f660504b7b943e7afaa863c3ca7
Sarah Al Bader <s.r.albader@gmail.com>
2019-11-28 20:23:10 +0300

    added styling

--- components/Authentication/Signup/index.js
+++ components/Authentication/Signup/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
 
+// Styling Components
+import { TextInput, TouchableOpacity, View } from "react-native";
+import styles from "./styles";
+
 // NativeBase Components
 import { Form, Item, Input, Button, Text } from "native-base";
 
@@ -19,26 +23,28 @@ class Signup extends Component {
 
   render() {
     return (
-      <Form>
-        <Item>
-          <Input
-            placeholder="Username"
-            autoCapitalize="none"
-            onChangeText={username => this.setState({ username })}
-          />
-        </Item>
-        <Item last>
-          <Input
-            placeholder="Password"
-            autoCapitalize="none"
-            secureTextEntry={true}
-            onChangeText={password => this.setState({ password })}
-          />
-        </Item>
-        <Button full onPress={this.handlePress}>
-          <Text>Signup</Text>
-        </Button>
-      </Form>
+      <View style={styles.authContainer}>
+        <Form>
+          <Item>
+            <Input
+              placeholder="Username"
+              autoCapitalize="none"
+              onChangeText={username => this.setState({ username })}
+            />
+          </Item>
+          <Item last>
+            <Input
+              placeholder="Password"
+              autoCapitalize="none"
+              secureTextEntry={true}
+              onChangeText={password => this.setState({ password })}
+            />
+          </Item>
+          <Button full onPress={this.handlePress}>
+            <Text>Signup</Text>
+          </Button>
+        </Form>
+      </View>
     );
   }
 }
